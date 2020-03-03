f0ab50dedfcb6bfd4d885756a77056724d163631
aakkaakkii <aakkaakkii1997@gmail.com>
2020-03-03 19:02:42 +0400

    fix script

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,36 +1,43 @@
 create sequence hibernate_sequence start 1 increment 1;
 
-create table user_role (
+create table user_role
+(
     user_id int8 not null,
-    roles varchar(255)
-    );
+    roles   varchar(255)
+);
 
-create table user_log (
-    id int8 not null,
-    action varchar(255),
-    date timestamp,
-    message varchar(2048),
+create table user_log
+(
+    id        int8 not null,
+    action    varchar(255),
+    date      timestamp,
+    message   varchar(2048),
     user_name varchar(255),
+    user_id   int8,
+    primary key (id)
+);
+
+create table user_tasks
+(
+    id      int8 not null,
+    tasks   int4 not null,
     user_id int8,
     primary key (id)
-    );
+);
 
-create table user_tasks (
-    id int8 not null,
-    tasks int4 not null,
-    user_id int8,
-    primary key (id)
-    );
-
-create table usr (
-    id int8 not null,
-    active boolean not null,
-    mail varchar(255),
+create table usr
+(
+    id       int8         not null,
+    active   boolean      not null,
+    mail     varchar(255),
     password varchar(255) not null,
     username varchar(255) not null,
     primary key (id)
-    );
+);
 
-alter table if exists user_role add constraint role_user_fk foreign key (user_id) references usr
-alter table if exists user_log add constraint log_user_fk foreign key (user_id) references usr
-alter table if exists user_tasks add constraint task_role_fk foreign key (user_id) references usr
+alter table if exists user_role
+    add constraint role_user_fk foreign key (user_id) references usr;
+alter table if exists user_log
+    add constraint log_user_fk foreign key (user_id) references usr;
+alter table if exists user_tasks
+    add constraint task_role_fk foreign key (user_id) references usr;
